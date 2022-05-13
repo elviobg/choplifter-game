03f309bccce5c57d5916f582be2eb84d0936c1a1
Elvio Batista <elvio.batista@pebmed.com.br>
2022-05-12 22:40:43 -0300

    apresenta elementos do jogo

---
+++ src/js/game.js
@@ -0,0 +1,7 @@
+function start() {
+	$("#start").hide();
+	$("#background").append("<div id='player'></div>");
+	$("#background").append("<div id='enemyTruck'></div>");
+	$("#background").append("<div id='enemyHelicopter'></div>");
+	$("#background").append("<div id='survivor'></div>");
+}
