b277d6df07a642fe1f6d2339ceeb97af968dbfbc
Elvio Batista <elvio.batista@pebmed.com.br>
2022-05-12 22:53:42 -0300

    adiciona animação de background

--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,26 @@
+const game = {
+  time: setInterval(loop, 30),
+};
+
 function start() {
-	$("#start").hide();
-	$("#background").append("<div id='player' class='playerAnimation'></div>");
-	$("#background").append("<div id='enemyTruck'></div>");
-	$("#background").append("<div id='enemyHelicopter' class='playerAnimation'></div>");
-	$("#background").append("<div id='survivor' class='survivorAnimation'></div>");
+  $("#start").hide();
+  $("#background").append("<div id='player' class='playerAnimation'></div>");
+  $("#background").append("<div id='enemyTruck'></div>");
+  $("#background").append(
+    "<div id='enemyHelicopter' class='playerAnimation'></div>"
+  );
+  $("#background").append(
+    "<div id='survivor' class='survivorAnimation'></div>"
+  );
 }
+
+function loop() {
+  updateScenary();
+}
+
+function updateScenary() {
+  const esquerda = parseInt($("#background").css("background-position"));
+  $("#background").css("background-position", esquerda - 1);
+}
+
+
